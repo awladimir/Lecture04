41e67c93adaf0311ccbdea87f7f307502bee268e
Vladimir <awladimir@yandex.ru>
2023-06-02 13:00:53 +0300

    решение задачи 13

--- HomeTask13/Program.cs
+++ HomeTask13/Program.cs
@@ -2,3 +2,18 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if ( n < 100)
+{
+    Console.WriteLine("У числа " + n + " нет третьей цифры");
+}
+else
+{
+    while (n >=1000)
+    {
+        n = n / 10;
+    }
+    int thirdDigit = n % 10;
+    Console.WriteLine("Третья цифра числа : " + thirdDigit);
+}
